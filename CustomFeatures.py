cc0b9d8da795721a8419f6fd029a292bdb8677c4
Thomas Grivaz <thomas.grivaz@epfl.ch>
2016-11-14 16:35:23 +0100

    added some features

--- CustomFeatures.py
+++ CustomFeatures.py
@@ -1,38 +1,44 @@
 #----------------------------------------#
 # Function that computes custom features #
 #----------------------------------------#
+
+
 def CalculateFeatures(VideoEvents=[], ForumEvents=[]):
 
-	# Initialize features dict
-	Features = {}
+    # Initialize features dict
+    Features = {}
 
-	# Features for video events
-	if len(VideoEvents)>0:
+    # Features for video events
+    if len(VideoEvents)>0:
 
-		# Calculate custom features
-		# Keys: TimeStamp, EventType, VideoID, CurrentTime, OldTime, NewTime, SeekType, OldSpeed, NewSpeed
-		TimeStamps = VideoEvents['TimeStamp']
-		TimeStampDiffs = [x[0]-x[1] for x in zip(TimeStamps[1:],TimeStamps[:-1])]
-		DurationOfVideoActivity = TimeStamps[-1] - TimeStamps[0]
-		AverageVideoTimeDiffs = sum(TimeStampDiffs)/max(1,len(TimeStampDiffs))
-		
-		# Append features to dictionary
-		Features.update({
-			'DurationOfVideoActivity' : DurationOfVideoActivity,
-			'AverageVideoTimeDiffs' : AverageVideoTimeDiffs
-		})
+        # Calculate custom features
+        # Keys: TimeStamp, EventType, VideoID, CurrentTime, OldTime, NewTime, SeekType, OldSpeed, NewSpeed
+        TimeStamps = VideoEvents['TimeStamp']
+        TimeStampDiffs = [x[0]-x[1] for x in zip(TimeStamps[1:],TimeStamps[:-1])]
+        DurationOfVideoActivity = TimeStamps[-1] - TimeStamps[0]
+        AverageVideoTimeDiffs = sum(TimeStampDiffs)/max(1,len(TimeStampDiffs))
+        NumberVideoWatched = len(set(VideoEvents['VideoID']))
 
-	# Features for forum events
-	if len(ForumEvents)>0:
+        # Append features to dictionary
+        Features.update({
+            'DurationOfVideoActivity': DurationOfVideoActivity,
+            'AverageVideoTimeDiffs': AverageVideoTimeDiffs,
+            'NumberVideoWatched': NumberVideoWatched
+        })
 
-		# Calculate custom features
-		# Keys: TimeStamp, EventType, PostType, PostID, PostLength
-		EventTypes = ForumEvents['EventType']
-		NumberOfThreadViews = EventTypes.count('Forum.Thread.View')
+    # Features for forum events
+    if len(ForumEvents)>0:
 
-		# Append features to dictionary
-		Features.update({
-			'NumberOfThreadViews' : NumberOfThreadViews
-		})
+        # Calculate custom features
+        # Keys: TimeStamp, EventType, PostType, PostID, PostLength
+        EventTypes = ForumEvents['EventType']
+        NumberOfThreadViews = EventTypes.count('Forum.Thread.View')
+        NumberOfComments = EventTypes.count('Forum.Thread.PostOn')
 
-	return Features
+        # Append features to dictionary
+        Features.update({
+            'NumberOfThreadViews': NumberOfThreadViews,
+            'NumberOfComments': NumberOfComments
+        })
+
+    return Features
